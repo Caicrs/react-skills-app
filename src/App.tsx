d8abe56acdab197a32188e1140792365dcb35bb3
caicrs <caicrs.contact@gmail.com>
2023-05-14 17:00:27 -0300

    Provider - Context provider and homepage component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Homepage from './pages/Homepage';
+import { GeneralProvider } from './contexts/GeneralContext';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<GeneralProvider>
+			<div className='App'>
+				<Homepage />
+			</div>
+		</GeneralProvider>
+	);
 }
 
 export default App;
