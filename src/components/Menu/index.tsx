53aa29bf7d7b54d8e3ed26f475ed4ccb4c06cea7
caicrs <caicrs.contact@gmail.com>
2023-05-14 16:54:42 -0300

    Menu - Main component for switch to others components

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,29 @@
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { useGeneral } from '../../contexts/GeneralContext';
+
+function Menu() {
+	const { setStage, stage } = useGeneral();
+
+	return (
+		<ButtonGroup aria-label='Basic example'>
+			<Button
+				onClick={() => setStage(1)}
+				variant={stage === 1 ? 'primary' : 'secondary'}>
+				UseEffect<br></br>VS <br></br>UseLayoutEffect
+			</Button>
+			<Button
+				onClick={() => setStage(2)}
+				variant={stage === 2 ? 'primary' : 'secondary'}>
+				Context<br></br>VS <br></br>useState
+			</Button>
+			<Button
+				onClick={() => setStage(3)}
+				variant={stage === 3 ? 'primary' : 'secondary'}>
+				Git
+			</Button>
+		</ButtonGroup>
+	);
+}
+
+export default Menu;
