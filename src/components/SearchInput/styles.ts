22bbd820d615c9a42871fd7a76c47b0369e1acc9
caicrs <caicrs.contact@gmail.com>
2023-05-16 20:29:36 -0300

    Styles - SearchInput component style file created

---
+++ src/components/SearchInput/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	background: white;
+	display: flex;
+	border-radius: 6px;
+`;
+
+export const Input = styled.input`
+	border: none;
+	padding-left: 12px;
+	border-radius: 6px;
+	outline: none;
+`;
