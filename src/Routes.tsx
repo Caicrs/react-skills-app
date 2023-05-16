1004bcbe45866e66f310a041db34c630b3ff92b6
caicrs <caicrs.contact@gmail.com>
2023-05-16 20:30:55 -0300

    Routes - Routes file created

---
+++ src/Routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+
+const DefaultRoutes = () => {
+
+    return(
+        <Routes>
+            <Route path="/" element={<Homepage />} />
+        </Routes>
+    )
+};
+
+export default DefaultRoutes;
