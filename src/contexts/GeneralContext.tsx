9e84f785500b6967eb62effa935cff84ecfff8ee
caicrs <caicrs.contact@gmail.com>
2023-05-16 20:35:18 -0300

    Context - Loading component added

--- src/contexts/GeneralContext.tsx
+++ src/contexts/GeneralContext.tsx
@@ -1,8 +1,11 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import Loading from '../components/Loading';
 
 interface GeneralContextData {
 	stage: number;
 	setStage: React.Dispatch<React.SetStateAction<number>>;
+	isLoading: boolean;
+	setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const GeneralContext = createContext<GeneralContextData>(
@@ -16,13 +19,18 @@ interface GeneralProps {
 
 export const GeneralProvider: React.FC<GeneralProps> = ({ children }) => {
 	const [stage, setStage] = useState<number>(1);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+
 	// Joining all variables that we want to export
 	return (
 		<GeneralContext.Provider
 			value={{
 				stage,
 				setStage,
+				isLoading,
+				setIsLoading,
 			}}>
+			{isLoading && <Loading />}
 			{children}
 		</GeneralContext.Provider>
 	);
