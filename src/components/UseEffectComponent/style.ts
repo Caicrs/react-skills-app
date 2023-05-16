01c9ba3b04264574afc8c31e22f9c44ba6a6d145
caicrs <caicrs.contact@gmail.com>
2023-05-16 20:29:21 -0300

    Styles - UseEffectComponent style file created

---
+++ src/components/UseEffectComponent/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	color: white;
+	max-height: 400px;
+	gap: 24px;
+	overflow-y: scroll;
+	padding: 0 10%;
+`;
+
+export const Text = styled.div`
+	width: 80%;
+	height: 100%;
+`;
