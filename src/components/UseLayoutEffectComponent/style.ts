d27cec857d7791565a8f7950ba56823604e4003c
caicrs <caicrs.contact@gmail.com>
2023-05-16 20:28:55 -0300

    Styles - UseLayoutEffectComponent style file created

---
+++ src/components/UseLayoutEffectComponent/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	color: white;
+	padding: 0 10%;
+`;
+
+export const Text = styled.div`
+	width: 80%;
+	height: 100%;
+`;
