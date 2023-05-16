b21ec406f6fd976381ee315d9129bff4052e4576
caicrs <caicrs.contact@gmail.com>
2023-05-16 20:30:00 -0300

    Styles - Stage 3 component style file created

---
+++ src/components/MainStages/stage3/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	display: flex;
+	max-height: 400px;
+	flex-direction: column;
+	align-items: center;
+	gap: 24px;
+	overflow-y: scroll;
+`;
+
+export const Box = styled.div`
+	width: fit-content;
+	color: white;
+`;
+
+export const List = styled.ul`
+	padding-top: 24px;
+	display: flex;
+	max-height: 400px;
+	flex-direction: column;
+	align-items: center;
+	gap: 12px;
+`;
+
+export const Item = styled.div`
+	max-width: 50%;
+	display: flex;
+	padding-bottom: 12px;
+	flex-direction: column;
+	align-items: center;
+	border-bottom: 1px solid rgba(255, 255, 255, 0.25);
+
+	> img {
+		padding-top: 12px;
+	}
+`;
