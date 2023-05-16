30189f1bd41b2785b94cfac2417fba20887e65f8
caicrs <caicrs.contact@gmail.com>
2023-05-16 20:28:37 -0300

    Styles - Homepage style file created

---
+++ src/pages/Homepage/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	display: flex;
+	justify-content: flex-end;
+	flex-direction: column;
+	gap: 24px;
+	height: 100%;
+`;
