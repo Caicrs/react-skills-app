e1d7d78e84b3348028aa4e1ecfff1dddac712df8
caicrs <caicrs.contact@gmail.com>
2023-05-16 20:39:54 -0300

    MainStages - New stage added

--- src/components/MainStages/index.tsx
+++ src/components/MainStages/index.tsx
@@ -1,21 +1,42 @@
-import styled from 'styled-components';
-import StageComponents from './stages';
+import { useGeneral } from '../../contexts/GeneralContext';
+import Stage1 from './stage1/stage_1';
+import Stage2 from './stage2/stage_2';
+import Stage3 from './stage3/stage_3';
+import Stage_4 from './stage4';
+import * as S from './styles';
 
-const ShowRoom = () => {
-	return (
-		<Container>
-			<StageComponents />
-		</Container>
-	);
+const StageComponents = () => {
+	const { stage } = useGeneral();
+	switch (stage) {
+		case 1:
+			return (
+				<S.Container>
+					<Stage1 />
+				</S.Container>
+			);
+		case 2:
+			return (
+				<S.Container>
+					<Stage2 />
+				</S.Container>
+			);
+		case 3:
+			return (
+				<S.Container>
+					<Stage3 />
+				</S.Container>
+			);
+		case 4:
+			return (
+				<S.Container>
+					<Stage_4 />
+				</S.Container>
+			);
+		default:
+			return <div>Invalid stage number</div>;
+	}
+
+	return null;
 };
 
-export default ShowRoom;
-
-const Container = styled.div`
-	background: #5c636a;
-	border-radius: 24px;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-`;
+export default StageComponents;
