b440104011675c6a384a4b1d4cc74222d7f7bd07
caicrs <caicrs.contact@gmail.com>
2023-05-16 20:30:33 -0300

    Styles - MainStage component style file created

---
+++ src/components/MainStages/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	background: #5c636a;
+	border-radius: 24px;
+	padding: 5% 0;
+
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+`;
