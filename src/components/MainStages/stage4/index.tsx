9d76ba21507a9100f3719cf273c68187a1770bf1
caicrs <caicrs.contact@gmail.com>
2023-05-17 22:09:11 -0300

    Pixabay - LikeImage function, navigation for imageById and like button added

--- src/components/MainStages/stage4/index.tsx
+++ src/components/MainStages/stage4/index.tsx
@@ -4,10 +4,13 @@ import SearchInput from '../../SearchInput';
 import * as S from './styles';
 import { hitsProps } from '../../../lib/models';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Stage_4 = () => {
 	const { setIsLoading } = useGeneral();
 	const [data, setData] = useState<hitsProps[]>([]);
+	const navigate = useNavigate();
+	const [likedImages, setLikedImages] = useState<number[]>([]);
 
 	const handleGetData = async (text: string) => {
 		setIsLoading(true);
@@ -18,22 +21,59 @@ const Stage_4 = () => {
 		setIsLoading(false);
 	};
 
+	const likeImage = (id: number) => {
+		const check = likedImages.filter((item: number) => item === id);
+		if (check.length > 0) {
+			const newData = likedImages.filter((item: number) => item !== id);
+			setLikedImages(newData);
+		} else {
+			setLikedImages((lastData) => [...lastData, id]);
+		}
+	};
+
 	return (
 		<S.Container>
 			<SearchInput
 				placeholder='Buscar imagens...'
 				onSearch={(e: string) => handleGetData(e)}
 			/>
-			{data &&
-				data.map((item) => (
-					<S.Block>
-						<img src={item.webformatURL} />
-						<div>
-							<S.Text>Views: {item.views}</S.Text>
-							<S.Text>Likes: {item.likes}</S.Text>
-						</div>
-					</S.Block>
-				))}
+			<S.ImagesContainer>
+				{data &&
+					data.map((item) => (
+						<S.Block>
+							<S.Content
+								onClick={() =>
+									navigate(
+										`/images/${item.webformatURL.substring(
+											24,
+											item.webformatURL.length
+										)}/${item.views}/${item.downloads}/${item.likes}/${
+											item.comments
+										}/${item.user}`
+									)
+								}>
+								<img alt='' src={item.webformatURL} />
+
+								<div>
+									<S.Text>Views: {item.views}</S.Text>
+									<S.Text>Likes: {item.likes}</S.Text>
+								</div>
+							</S.Content>
+
+							<S.LikeButton
+								style={
+									likedImages.filter((items) => items === item.id).length > 0
+										? { background: 'green' }
+										: {}
+								}
+								onClick={() => likeImage(item.id)}>
+								{likedImages.filter((items) => items === item.id).length > 0
+										? 'Remover curtida'
+										: 'Curtir'}
+							</S.LikeButton>
+						</S.Block>
+					))}
+			</S.ImagesContainer>
 		</S.Container>
 	);
 };
