bfd5c34bf7ba40ebe9a4872ee208f8a0b8590172
caicrs <caicrs.contact@gmail.com>
2023-05-17 22:07:02 -0300

    Routes - ImageById page added

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
+import ImageById from './pages/Image';
 
 const DefaultRoutes = () => {
-
-    return(
-        <Routes>
-            <Route path="/" element={<Homepage />} />
-        </Routes>
-    )
+	return (
+		<Routes>
+			<Route path='/' element={<Homepage />} />
+			<Route path='/images/:webformaturl/:views/:downloads/:likes/:comments/:user' element={<ImageById />} />
+		</Routes>
+	);
 };
 
 export default DefaultRoutes;
